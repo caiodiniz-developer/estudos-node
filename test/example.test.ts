9db050df88e9918a339d9d3efdd43fb1bb3e0194
Caio <cvdinizramos@gmail.com>
2026-08-17 19:10:38 -0300

    Testando criação de transação usando supertest

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,8 +1,22 @@
-import { expect, test } from "vitest";
+import { test, beforeAll, afterAll } from "vitest";
+import request from "supertest";
+import { app } from "../src/app.js";
 
-test("o usuario consegue criar uma nova transacao", () => {
-  //fazer chamada http para criar uma nova transacao
-  const responseStatusCode = 201;
+beforeAll(async () => {
+  await app.ready();
+});
 
-  expect(responseStatusCode).toEqual(201);
+afterAll(async () => {
+  await app.close();
+});
+
+test("User can create new transaction", async () => {
+  await request(app.server)
+    .post("/transactions")
+    .send({
+      title: "New transaction",
+      amount: 5000,
+      type: "credit",
+    })
+    .expect(201);
 });
