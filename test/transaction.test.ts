17ae79169297e99484a37176dafb58252e6bbbef
Caio <cvdinizramos@gmail.com>
2026-08-19 19:56:06 -0300

    criando beforeEach para rodar as migrates

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -1,6 +1,7 @@
-import { it, beforeAll, afterAll, describe, expect } from "vitest";
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from "vitest";
 import request from "supertest";
 import { app } from "../src/app.js";
+import { execSync } from "node:child_process";
 
 describe("Transactions routes", () => {
   beforeAll(async () => {
@@ -9,6 +10,10 @@ describe("Transactions routes", () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:latest");
   });
 
   it("User can create new transaction", async () => {
