437ace889727428b3d725010f85a7fc6908b4077
Caio <cvdinizramos@gmail.com>
2026-08-18 21:35:48 -0300

    criando teste de criar nova transacao

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -1,26 +1,38 @@
-import { test, beforeAll, afterAll, describe } from "vitest";
+import { it, beforeAll, afterAll, describe } from "vitest";
 import request from "supertest";
 import { app } from "../src/app.js";
 
-describe('Transactions routes', () =>{
-  
-})
+//todo teste deve se excluir d qualquer contexto, jamais escrever um teste que depende de outro teste
 
-beforeAll(async () => {
-  await app.ready();
-});
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
 
-afterAll(async () => {
-  await app.close();
-});
+  afterAll(async () => {
+    await app.close();
+  });
 
-test("User can create new transaction", async () => {
-  await request(app.server)
-    .post("/transactions")
-    .send({
-      title: "New transaction",
-      amount: 5000,
-      type: "credit",
-    })
-    .expect(201);
+  it("User can create new transaction", async () => {
+    await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      })
+      .expect(201);
+  });
+
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookie = createTransactionResponse.get("Set-Cookie");
+  });
 });
