589f6b2f8bb7d7b2d1504422ce04bc210829fffe
Caio <cvdinizramos@gmail.com>
2026-08-19 19:49:47 -0300

    configurando o dotenv no {config}

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import { z } from "zod";
+
+console.log(process.env);
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
